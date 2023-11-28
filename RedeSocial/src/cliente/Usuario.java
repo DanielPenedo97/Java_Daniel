50db6392fe89a7078a801d36b4b985760f0dff4e
Daniel97 <dapsantos.cic@uesc.br>
2023-11-28 17:09:09 -0300

    Exercicio Rede Social

--- RedeSocial/src/cliente/Usuario.java
+++ RedeSocial/src/cliente/Usuario.java
@@ -4,14 +4,14 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Usuario {
-	private String nome, email, nacionalidade;
+	private String nome, email, senha;
 	private ArrayList<String> postagens;
 	private int quantidadeDePostagens, pontuacao;
 	
 	public Usuario() {
 		this.nome = nome;
 		this.email = email;
-		this.nacionalidade = nacionalidade;
+		this.senha = senha;
 		postagens = new ArrayList<String>();
 		this.quantidadeDePostagens = 0;
 		this.pontuacao = 0;
@@ -33,12 +33,12 @@ public class Usuario {
 		this.email = email;
 	}
 
-	public String getNacionalidade() {
-		return nacionalidade;
+	public String getSenha() {
+		return senha;
 	}
 
-	public void setNacionalidade(String nacionalidade) {
-		this.nacionalidade = nacionalidade;
+	public void setSenha(String senha) {
+		this.senha = senha;
 	}
 	
 	public int getQuantidadeDePostagens() {
@@ -67,30 +67,5 @@ public class Usuario {
 	
 	public void alteraPontuacao(int delta) {
 	
-	}
-	
-	public static void main(String[] args) {
-		Usuario U1 = new Usuario();
-		String nome, email, nacionalidade;
-		
-		Scanner sc1 = new Scanner(System.in);
-		System.out.println("Digite o nome do seu Usuario:");
-		nome = sc1.nextLine();
-		U1.setNome(nome);
-		
-		Scanner sc2 = new Scanner(System.in);
-		System.out.println("Digite o email do seu Usuario:");
-		email = sc2.nextLine();
-		U1.setEmail(email);
-		
-		Scanner sc3 = new Scanner(System.in);
-		System.out.println("Digite a nacionalidade do seu Usuario:");
-		nacionalidade = sc3.nextLine();
-		U1.setNacionalidade(nacionalidade);
-		
-		String postagem = "";
-		U1.adicionaPostagem(postagem);
-		System.out.println("Usuario: "+ U1.getNome()+ "\n"+ "Email: "+ U1.getEmail()+ "\n"+ "Nacionalidade: "+ U1.getNacionalidade()+"\n");
-		System.out.println("Quantidade de postagens: "+ U1.getQuantidadeDePostagens());
 	}
 }
